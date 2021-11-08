dc387a4c53c39692a0f1d309175652aef1092aae
khanhzoball <khanhhnguyen@g.ucla.edu>
2021-11-07 21:52:07 -0800

    finished creating login and signup system

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,63 +1,96 @@
 const express = require("express")
 const mongoose = require("mongoose")
+const user = mongoose.model("user")
 const jwt = require("jsonwebtoken")
-
 const router = express.Router()
-const User = mongoose.model("User")
 const secret = process.env.SECRET
+const login_verif = require("../middlewares/login_verif.js")
 
+// Default homepage
 router.get("/", (request, response) => {
     response.send("wassup")
 })
 
-// User signup
+router.get("/protected", login_verif, (request, response) => {
+    response.send("Access Cranted")
+})
+
+// user signup for POST
 router.post("/signup", (request, response) => {
     const username = request.body.username;
     const password = request.body.password;
 
+    // If missing a field: error
     if (!username || !password) {
         response.status(422).json({ error: "Please add all fields" })
     }
 
-    User.findOne({ username: username })
-    .then ((savedUser) => {
-        if (savedUser) {
-            return response.status(422).json({ error: "There already exists an account with that username"})
+    // Checks if there already exists an account with that username
+    user.findOne({ username: username })
+    .then ((saved_user) => {
+        if (saved_user) {
+            return response.status(422).json({ error: "There already exists an account with that username" })
         }
-    })
+        // Creates a new user and saves the account
+        const new_user = new user({ username, password })
 
-    const user = new User({ username, password })
-    user.save()
-    .then(user => {
-        response.json({ message: "Account Saved Sucessfully"})
+        new_user.save()
+        .then(() => {
+            response.json({ message: "Account Saved Sucessfully" })
+        })
+        .catch(error => {
+            console.log(error)
+        })
     })
     .catch(error => {
         console.log(error)
     })
+
 })
 
-// User log in
+// user log in for POST
 router.post("/login", (request, response) => {
     const username = request.body.username;
     const password = request.body.password;
 
+    // Checks if missing a field
     if (!username || !password) {
         response.status(422).json({ error: "Please include both username and password" })
     }
 
-    User.findOne({ username: username })
-    .then ((savedUser) => {
-        if (!savedUser) {
+    user.findOne({ username: username })
+    .then ((saved_user) => {
+        
+        // If username doesn't exist: error
+        if (!saved_user) {
             return response.status(422).json({ error: "Invalid username or password" })
         }
+        // Logs in if username and password matches
+        // Generates a token that allows user to access private information
+        else if (saved_user.password == password) {
+            const token = jwt.sign({ _id: saved_user._id }, secret)
 
-        if (savedUser.password == password) {
-            response.json("Successful")
-            const token = jwt.sign({ id: savedUser.id }, secret)
+            // For testing purposes
+            console.log(JSON.stringify({
+                token: token,
+                user: {
+                    username: username,
+                }
+            }))
+
+            response.json(({
+                token: token,
+                user: {
+                    username: username,
+                }
+            }))
 
         } else {
             return response.status(422).json({ error: "Invalid username or password" })
         }
+    })
+    .catch(error => {
+        console.log(error)
     })
 })
 
