dd29f6fe1239ac0a138131138641c1cf78d2abda
khanhzoball <khanhhnguyen@g.ucla.edu>
2021-11-07 22:14:06 -0800

    created navbar'

---
+++ frontend/src/components/navbar.js
@@ -0,0 +1,17 @@
+import React from "react"
+import {Link} from "react-router-dom"
+
+const Navbar = () => {
+    return(
+        <nav>
+            <div className="topnav">
+                <Link to="/">Home</Link>
+                <Link to="/login">Login</Link>
+                <Link to="/signup">Signup</Link>
+                <Link to="/menu">Menu</Link>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
