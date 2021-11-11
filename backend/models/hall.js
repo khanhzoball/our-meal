65c78a3bc23bcc0a902c3932f515b8d7e4fe8251
khanhzoball <khanhhnguyen@g.ucla.edu>
2021-11-11 03:16:13 -0800

    modified hallSchema's likes and comment property

---
+++ backend/models/hall.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose")
+
+var hallSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    likes: {
+        type: Number,
+    },
+    comments: {
+        type: Array,
+    }
+})
+
+mongoose.model("hall", hallSchema)
