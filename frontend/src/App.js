d624f154dd76ce1ba8e837961e291ea3dd244131
khanhzoball <khanhhnguyen@g.ucla.edu>
2021-12-01 21:56:08 -0800

    Changed Title name of web page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,8 @@ import React from "react";
 import "./App.css";
 import Navbar from "./components/navbar.js";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Helmet } from "react-helmet";
+
 import Home from "./components/home.js";
 import Signup from "./components/signup.js";
 import Login from "./components/login.js";
@@ -12,16 +14,21 @@ import "./components/comp.css";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Navbar/>
-      <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/signup" element={<Signup/>}/>
-        <Route path="/login" element={<Login/>}/>
-        <Route path="/menu" element={<Menu/>}/>
-        <Route path="/reviews" element={<Reviews/>}/>
-      </Routes>
-    </BrowserRouter>
+    <div className="application">
+      <Helmet>
+        <title>OurMeal</title>
+      </Helmet>
+      <BrowserRouter>
+        <Navbar/>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/signup" element={<Signup/>}/>
+          <Route path="/login" element={<Login/>}/>
+          <Route path="/menu" element={<Menu/>}/>
+          <Route path="/reviews" element={<Reviews/>}/>
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
 
